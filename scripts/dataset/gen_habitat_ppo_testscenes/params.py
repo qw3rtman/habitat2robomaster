739885c8feeb31dac0b94a43bfa65aae3d3ff07a
Nimit Kalra <nimit@utexas.edu>
2020-04-04 03:15:53 -0500

    fix scripts

--- scripts/dataset/gen_habitat_ppo_testscenes/params.py
+++ scripts/dataset/gen_habitat_ppo_testscenes/params.py
@@ -3,15 +3,15 @@ from pathlib import Path
 
 jobs = list()
 
-job = f"""python habitat_wrapper.py \\
---input_type depth \\
---dataset_dir /scratch/cluster/nimit/data/habitat/ppo_depth \\
---num_episodes 1000
+for input_type, num_episodes in [('rgb', 1500), ('depth', 2000)]:
+    job = f"""python habitat_wrapper.py \\
+    --input_type {input_type} \\
+    --dataset_dir /scratch/cluster/nimit/data/habitat/ppo_{input_type}  \\
+    --num_episodes {num_episodes}
 """
 
-jobs.append(job)
-print(job)
+    jobs.append(job)
+    print(job)
 
-np.random.shuffle(jobs)
 print(len(jobs))
 JOBS = jobs[:len(jobs)]
--- scripts/dataset/gen_habitat_ppo_testscenes/run_gen_habitat_ppo_testscenes_2.sh
+++ scripts/dataset/gen_habitat_ppo_testscenes/run_gen_habitat_ppo_testscenes_2.sh
@@ -7,6 +7,6 @@ cd $HOME/Documents/robomaster/habitat2robomaster
 conda env list
 conda activate /scratch/cluster/nimit/miniconda3/envs/robo
 python habitat_wrapper.py \
-    --input_type rgb \
-    --dataset_dir /scratch/cluster/nimit/data/habitat/ppo_rgb  \
-    --num_episodes 1500
+    --input_type depth \
+    --dataset_dir /scratch/cluster/nimit/data/habitat/ppo_depth  \
+    --num_episodes 2000
