cbff970bb23f6ba3c078182d8c492a706ea27978
Nimit Kalra <nimit@utexas.edu>
2020-04-12 00:21:56 -0500

    interpolate dataset

--- habitat_dataset.py
+++ habitat_dataset.py
@@ -16,7 +16,7 @@ from utils import StaticWrap, DynamicWrap
 ACTIONS = torch.eye(4)
 
 
-def get_dataset(dataset_dir, dagger=False, capacity=2000, batch_size=128, num_workers=4, **kwargs):
+def get_dataset(dataset_dir, dagger=False, interpolate=False, capacity=2000, batch_size=128, num_workers=4, **kwargs):
 
     def make_dataset(is_train):
         data = list()
@@ -26,7 +26,7 @@ def get_dataset(dataset_dir, dagger=False, capacity=2000, batch_size=128, num_wo
         num_episodes = int(max(1, kwargs.get('dataset_size', 1.0) * len(episodes)))
 
         for episode_dir in episodes[:num_episodes]:
-            data.append(HabitatDataset(episode_dir, is_seed=dagger))
+            data.append(HabitatDataset(episode_dir, is_seed=dagger, interpolate=interpolate))
 
         print('%s: %d' % (train_or_val, len(data)))
 
@@ -39,7 +39,7 @@ def get_dataset(dataset_dir, dagger=False, capacity=2000, batch_size=128, num_wo
 
 
 class HabitatDataset(torch.utils.data.Dataset):
-    def __init__(self, episode_dir, is_seed=False):
+    def __init__(self, episode_dir, is_seed=False, interpolate=False):
         if not isinstance(episode_dir, Path):
             episode_dir = Path(episode_dir)
 
@@ -49,21 +49,47 @@ class HabitatDataset(torch.utils.data.Dataset):
 
         self.episode_dir = episode_dir
 
-        self.imgs = list(sorted(episode_dir.glob('rgb_*.png')))
-        self.segs = list(sorted(episode_dir.glob('seg_*.npy')))
-
         self.measurements = pd.read_csv(episode_dir / 'episode.csv')
-        self.positions = np.stack(itemgetter('x','y','y')(self.measurements), -1)
-        self.rotations = np.stack(itemgetter('i','j','k','l')(self.measurements), -1)
-        self.actions = torch.LongTensor(self.measurements['action'])
+
+        _indices = np.ones(len(self.measurements), dtype=np.bool)
+        if interpolate:
+            for i in range(len(_indices)):
+                if i % 5 in [0, 1, 4] and i + 1 < len(_indices):
+                    _indices[i] = True
+                else:
+                    _indices[i] = False
+
+            self.imgs = [rgb for i, rgb in enumerate(sorted(episode_dir.glob('rgb_*.png'))) if _indices[i]]
+            self.segs = [seg for i, rgb in enumerate(sorted(episode_dir.glob('seg_*.npy'))) if _indices[i]]
+        else:
+            self.imgs = list(sorted(episode_dir.glob('rgb_*.png')))
+            self.segs = list(sorted(episode_dir.glob('seg_*.npy')))
+
+        self.positions = np.stack(itemgetter('x','y','y')(self.measurements), -1)[_indices]
+        self.rotations = np.stack(itemgetter('i','j','k','l')(self.measurements), -1)[_indices]
+
+        if interpolate:
+            _action_indices = []
+            for _index, include in enumerate(_indices):
+                if not include:
+                    continue
+                if _index % 5 == 0:
+                    _action_indices.append(_index)
+                if _index % 5 == 1:
+                    _action_indices.append(_index - 1)
+                if _index % 5 == 4:
+                    _action_indices.append(_index + 1)
+
+            self.actions = torch.LongTensor(self.measurements['action'])[_action_indices]
+            self.measurements = self.measurements[_indices]
+        else:
+            self.actions = torch.LongTensor(self.measurements['action'])
 
         self.info = pd.read_csv(episode_dir / 'info.csv').iloc[0]
         self.start_position = torch.Tensor(itemgetter('start_pos_x', 'start_pos_y', 'start_pos_z')(self.info))
         # NOTE: really a quaternion
         self.start_rotation = torch.Tensor(itemgetter('start_rot_i', 'start_rot_j', 'start_rot_k', 'start_rot_l')(self.info))
         self.end_position   = torch.Tensor(itemgetter('end_pos_x', 'end_pos_y', 'end_pos_z')(self.info))
-
-        self.transform  = transforms.ToTensor()
 
     def __len__(self):
         return len(self.imgs)
