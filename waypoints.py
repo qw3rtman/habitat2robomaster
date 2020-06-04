3291edf8ef1ea10e19a0b8f22c35a095db9074ff
Nimit Kalra <nimit@utexas.edu>
2020-06-04 05:10:48 -0500

    projection working

--- waypoints.py
+++ waypoints.py
@@ -25,12 +25,14 @@ t = np.arccos(c)
 cv2.namedWindow('rgb', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('rgb', 768, 320)
 
+f = 384 / (2 * np.tan(120 * np.pi / 360))
+A = torch.tensor([[1.675*f,   0.0000, 192.0000],
+                  [  0.0000, 0.875*f,  0.0000],
+                  [  0.0000,   0.0000,   1.0000]])
+
 i = 0
 init = False
 while i < replay_buffer.size:
-    rgb = replay_buffer.targets[i].squeeze().numpy()
-    cv2.imshow('rgb', cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB))
-
     movement = replay_buffer.actions[i:i+8] == 1
     k = np.searchsorted(np.cumsum(movement), 5)
 
@@ -40,14 +42,14 @@ while i < replay_buffer.size:
     ax.set_xlim(-1,1)
 
     R = r[i:i+k][movement[:k]]-r[i] # relative
-    R = -0.35 * np.abs(R / R.mean())
+    R = -0.35 * np.abs(R / R.abs().mean())
     T = t[i:i+k][movement[:k]]-t[i+1] # absolute
 
     x, y = rotate_origin_only(R*np.cos(T), R*np.sin(T), np.pi/2)
-    print(T.shape)
     if T.shape[0] > 1 and (y < 0).sum() == 0: # if behind camera, then use prev
         _t = np.linspace(T[0], T[-1], 10)
         _r = np.linspace(R[0], R[-1], 10)
+        print(R[-1])
         _x, _y = rotate_origin_only(_r*np.cos(_t), _r*np.sin(_t), np.pi/2)
         init = True
 
@@ -60,6 +62,15 @@ while i < replay_buffer.size:
     plt.savefig('test.png')
     plt.close()
     cv2.imshow('bird', np.array(Image.open('test.png')))
+
+    rgb = replay_buffer.targets[i].squeeze().numpy()
+    if init:
+        M = torch.FloatTensor(np.stack([_x, _y, np.ones(_y.shape[0])]))
+        uv = torch.mm(A, M)[:2]
+
+        for u,v in np.uint32(uv.numpy()).T:
+            cv2.circle(rgb, (u, 160-v), 2, (255, 0, 0), -1)
+    cv2.imshow('rgb', cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB))
 
     key = cv2.waitKey(0)
     if key == 97:
